2e093ed8e41c96ed0cc21c7ca8c1811f7fcae82f
Viktoriia-Tymets <vika777tymets@gmail.com>
2025-02-23 14:01:04 +0000

    need to changee everything

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+import avatar from "../../friends.json";
+
+
+
+export default function FriendListItem({
+    friend: {avatar, name, isOnline}
+}) {
+
+const statusClsx = clsx(css.text, isOnline ? css.online : css.offline);
+
+    return (
+    <div>
+  <img src={avatar} alt="Avatar" width="48" />
+  <p>{name}</p>
+  <p>{isOnline}</p>
+</div>
+)
+}
